eca572a0a0d720b518f1745cb149365831b0664c
anxpham <116184620+anxpham@users.noreply.github.com>
2024-05-13 22:37:43 -0700

    Update draw.py

--- draw.py
+++ draw.py
@@ -1,6 +1,6 @@
 import pygame
 
-def redraw_window(WIN, BG, main_font, level, lives, lost, lost_font, WIDTH, enemies, player):
+def redraw_window(WIN, BG, main_font, level, lives, lost, win, result_font, WIDTH, enemies, player):
     WIN.blit(BG, (0, 0))
     lives_label = main_font.render(f"Lives: {lives}", 1, (255, 255, 255))
     level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
@@ -10,6 +10,10 @@ def redraw_window(WIN, BG, main_font, level, lives, lost, lost_font, WIDTH, enem
         enemy.draw(WIN)
     player.draw(WIN)
     if lost:
-        lost_label = lost_font.render("You Lost!!", 1, (255, 255, 255))
+        lost_label = result_font.render("You Lost!!", 1, (255, 255, 255))
         WIN.blit(lost_label, (WIDTH / 2 - lost_label.get_width() / 2, 350))
+    if win:
+        win_label = result_font.render("You Win!!", 1, (255, 255, 255))
+        WIN.blit(win_label, (WIDTH / 2 - win_label.get_width() / 2, 350))
+
     pygame.display.update()
